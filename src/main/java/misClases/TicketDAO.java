c4254edd1c693f2ebe82d789ea58e09709b67896
Adriano Caloni <caloniadrianoj@gmail.com>
2023-12-16 02:26:16 -0300

    Se creo el metodo para LISTAR tickets

--- src/main/java/misClases/TicketDAO.java
+++ src/main/java/misClases/TicketDAO.java
@@ -1,11 +1,67 @@
 //TODA LA LOGICA DE NEGOCIO VA EN ESTA CLASE
 package misClases;
 
-public class TicketDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class TicketDAO 
+{
+	Connection conexion=null;
+	
+	public TicketDAO () throws ClassNotFoundException 
+	{
+		Conexion con=new Conexion();
+		conexion=con.getConnection();
 	}
+	
+	//LISTAR
+	public List<Ticket> listarTickets()
+	{
+		PreparedStatement ps;
+		ResultSet rs;
+		List<Ticket> lista= new ArrayList<>();
+		
+		try 
+		{
+			ps=conexion.prepareStatement("SELECT * FROM Tickets");
+			rs= ps.executeQuery();
+			
+			while(rs.next()) 
+			{
+				int id=rs.getInt("id");
+				String nombre = rs.getString("nombre");
+				String apellido = rs.getString("apellido");
+				String mail = rs.getString("mail");
+				int cant=rs.getInt("cant");
+				int tipo_ticket=rs.getInt("tipo_ticket");
+				float total_facturado=rs.getFloat("total_facturado");
+				
+				Ticket ticket=new Ticket(id,nombre,apellido,mail,cant,tipo_ticket,total_facturado);
+				lista.add(ticket);
+				
+			}
+			
+			return lista;
+		} 
+		catch (SQLException e)
+		{
+			System.out.println(e);
+			return null;
+			
+		}
+		
+		
+		
+	}
+	
+	//INSERTAR
+	
+	//ELIMINAR
+	
 
 }
